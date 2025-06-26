cb003b5c3a7994fb8b1fc52ca9d1505ba58dcfb8
MrMuhammadjon <asliyevmukhammadjon@gmail.com>
2025-06-26 13:26:15 +0500

    Refactor code structure for improved readability and maintainability

--- src/components/PageComponents/HomeBanner.jsx
+++ src/components/PageComponents/HomeBanner.jsx
@@ -1,15 +1,37 @@
 import React from 'react'
-import Spline from '@splinetool/react-spline';
+import HeadIphoneimg from '../../assets/iphone.png'
 
 const HomeBanner = () => {
-  return (
-    <div className='w-[100%] m-auto mt-[100px] lg:w-[80%] relative'>
-        <h1 className=''>
-            I
-        </h1>
-      <Spline scene="https://prod.spline.design/UEAVdVLNf-FeQTdG/scene.splinecode" />
-    </div>
-  )
+    return (
+        <>
+            <section className="w-full bg-[#211C24] text-white pt-16">
+                <div className="w-[90%] max-w-[1400px] h-[500px] mx-auto flex flex-col lg:flex-row items-center justify-between gap-10">
+
+                    <div className="flex-1 space-y-6 text-center lg:text-left">
+                        <p className="text-lg text-gray-400 tracking-wide">Pro. Beyond.</p>
+                        <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold leading-tight">
+                            iPhone 14 <span className="text-purple-400">Pro</span>
+                        </h1>
+                        <p className="text-gray-300 max-w-md mx-auto lg:mx-0">
+                            Created to change everything for the better. For everyone.
+                        </p>
+                        <button className="mt-4 px-8 py-3 bg-purple-600 hover:bg-purple-700 transition rounded-full font-semibold shadow-lg">
+                            Shop Now
+                        </button>
+                    </div>
+
+                    <div className="flex-1 flex items-center justify-center w-full h-[100%] overflow-hidden">
+                        <img
+                            src={HeadIphoneimg}
+                            alt="iPhone 14"
+                            className="w-[500px]  drop-shadow-2xl transition-transform duration-500 hover:scale-105 mt-200 hover:-rotate-5 hover:tra"
+                        />
+                    </div>
+
+                </div>
+            </section>
+        </>
+    )
 }
 
 export default HomeBanner
